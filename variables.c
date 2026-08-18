17fdaeb2e2a8c63366a86b775421193fb4313f29
Mokshithareddy20 <mokshithareddy1317@gmail.com>
2026-08-18 23:14:21 +0530

    Add variables and format specifiers in variables.c

---
+++ variables.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+
+int main() {
+
+int age = 25;
+float gpa =8.5;
+double pi = 3.1415926535;
+char currency ='$';
+char name[] ="Mokshitha";
+/* We use char meaning a single character
+for more than one value use use [] it is called a Array of characters*/
+printf("%d\n",age);
+printf("%f\n",gpa);
+printf("%lf\n",pi);
+printf("%c\n",currency);
+printf("%s\n",name);
+
+return 0;
+}
+
